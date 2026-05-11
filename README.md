e07dc12df300a1c3f400234547ec330ecaf10c04
marcosjantsch <85318367+marcosjantsch@users.noreply.github.com>
2026-05-10 23:42:35 -0300

    1

--- README.md
+++
@@ -1,1 +0,0 @@
-# GFPIncendioV2
--- deployment/run_docker.ps1
+++
@@ -1,7 +0,0 @@
-$ErrorActionPreference = "Stop"
-
-if (-not (Test-Path ".\docker-compose.yml")) {
-    Write-Error "Execute este script na raiz do projeto."
-}
-
-docker compose up --build
